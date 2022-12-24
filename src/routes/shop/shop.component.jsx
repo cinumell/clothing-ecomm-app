9a48ef5f528ff1cc114b1dd3e3a04a9735b59d9d
Chaitanya Inumella <cinumell@asu.edu>
2022-12-24 16:28:49 -0700

    Added all the migration changes from contexts to redux

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,9 +1,26 @@
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
-import CategoriesPreview  from '../categories-preview/categories-preview.component';
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+
+import { setCategories } from '../../store/categories/categories.action';
+
+import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 
 const Shop = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categories = await getCategoriesAndDocuments('categories');
+      dispatch(setCategories(categories));
+    };
+
+    getCategoriesMap();
+  }, []);
+
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
@@ -13,4 +30,3 @@ const Shop = () => {
 };
 
 export default Shop;
-
